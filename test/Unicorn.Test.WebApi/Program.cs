6fcbe8b4a14a0fc9694e02bffe7b04290538ad1e
stulzq <stulzq@qq.com>
2018-11-12 14:21:17 +0800

    [Fix] namespace error

--- test/Unicorn.Test.WebApi/Program.cs
+++ test/Unicorn.Test.WebApi/Program.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Unicorn.Middleware.RealIp;
+using Unicorn.AspNetCore.Middleware.RealIp;
 
 namespace Unicorn.Test.WebApi
 {
